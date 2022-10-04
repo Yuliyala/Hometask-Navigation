d380044084c8ccc6224b3221bdab3550b613c302
Yuliyala <114910133+Yuliyala@users.noreply.github.com>
2022-10-04 19:00:13 +0300

    Added  DatePicker

---
+++ Hometask Navigation/Hometask Navigation/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  Hometask Navigation
+//
+//  Created by Yuliya Lapenak on 10/4/22.
+//
+
+import UIKit
+
+struct Person {
+    
+    var name: String
+    var surname: String
+    var birthday: String
+}
+
--- Hometask Navigation/Hometask Navigation/ViewController.swift
+++ Hometask Navigation/Hometask Navigation/ViewController.swift
@@ -13,14 +13,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var surnameTextField: UITextField!
     @IBOutlet weak var birthdayTextField: UITextField!
     
+    let datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nameTextField.placeholder = "Name"
         surnameTextField.placeholder = "Surname"
         birthdayTextField.placeholder = "Date of birth"
+        
+        createDatePicker()
     }
 
-
+    func createDatePicker() {
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+        birthdayTextField.inputView = datePicker
+        birthdayTextField.inputAccessoryView = createToolBar()
+    }
+    
+    func createToolBar() -> UIToolbar {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil , action: #selector(donePressed))
+        toolbar.setItems([doneButton], animated: true)
+        return toolbar
+    }
+    
+    @objc func donePressed() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        self.birthdayTextField.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+        
+    }
 }
 
