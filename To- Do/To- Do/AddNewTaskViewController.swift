3a178db7c0ea0526ecf8f6f999f982a2c2dff7cf
Yuliyala <114910133+Yuliyala@users.noreply.github.com>
2022-10-06 20:33:45 +0300

    [1] Added TableView,  added AddNewTaskViewController

---
+++ To- Do/To- Do/AddNewTaskViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddNewTaskViewController.swift
+//  To- Do
+//
+//  Created by Yuliya Lapenak on 10/6/22.
+//
+
+import UIKit
+
+class AddNewTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ To- Do/To- Do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To- Do
+//
+//  Created by Yuliya Lapenak on 10/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
